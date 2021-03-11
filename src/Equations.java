4922d23a8d4f0b461e776856f2ce1098b8cc82f6
Stephen Chen <stephenc1999@gmail.com>
2021-03-11 14:36:59 -0500

    didajso

---
+++ src/Equations.java
@@ -0,0 +1,17 @@
+
+import java.lang.Math;
+
+class Equations {
+
+    private float EuclDist(float a, float b) {
+        float dist = Math.sqrt(a*a+b*b);
+        return dist;
+    }
+
+    private float EnergyCost(float velocity) {
+        float speed = EuclDist(velocity.get(1),velocity.get(2));
+        float energyCost = speed; //Equation to calculate energy cost based on speed
+        return energyCost;
+    }
+
+}
---
+++ src/Lion.java
@@ -0,0 +1,22 @@
+
+//Created: 3/11/2021
+//Main class for Lion (predator)
+
+class Lion {
+
+    private float speed;
+    private int energy;
+    private float detectRange;
+    private ArrayList<Zebra> inRange;
+
+    public class Lion() {
+
+    }
+
+    private void detectPrey(ArrayList<Zebra> zebraList) {
+        for (x:zebraList) { //for each thing in list
+
+        }
+    }
+
+}
